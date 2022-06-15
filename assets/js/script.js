db9f24cde54dac98e59d63d8a0eb75eec38ba639
Brandon Davis <brandon.davis@conehealth.com>
2022-06-15 09:33:51 -0400

    pseudo coding password generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var generatePassword = function() {
+  //prompt user for complexity options to user
+
+  //based on options and password length randomly choose how many of each character type goes into password
+
+  //generate random characters (consider an object that contains that different options), hav methods to help select
+
+  //push all into an array and randomize
+
+  return password;
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
