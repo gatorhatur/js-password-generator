f0fccf9e54e816914bfe614d8a5cf49d20e90e5b
Brandon Davis <brandon.davis@conehealth.com>
2022-06-15 12:18:06 -0400

    added logic and prompts for password criteria and created passwordCriteria Obj

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordCriteria = {
+    length: 7,
+    lower_case: false,
+    upper_case: false,
+    numbers: false,
+    sp_char: false,
+    minCriteria: function(){
+      if(!this.lower_case && !this.upper_case && !this.numbers && !this.sp_char ){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+};
+
+console.log(passwordCriteria);
+
 var generatePassword = function() {
   //prompt user for complexity options to user
+  while(passwordCriteria.length < 8){
+    passwordCriteria.length = window.prompt("How long would you like your password to be? Choose a number between 8 and 128.");
+  }
+
+  while(!passwordCriteria.minCriteria()){
+  window.alert("Passwords must use at least one of the criteria. You will be asked these questions again if you click cancel, to all");
+  passwordCriteria.lower_case = window.confirm("Would you like to use lower case letters?");
+  passwordCriteria.upper_case = window.confirm("Woudl you like to use upper case letters?");
+  passwordCriteria.numbers = window.confirm("Would you like to include numbers?");
+  passwordCriteria.sp_char = window.confirm("Would you like to use special characters? ex. ?,/,-,*,#,@,$");
+  }
+
+
 
   //based on options and password length randomly choose how many of each character type goes into password
 
@@ -12,7 +43,7 @@ var generatePassword = function() {
 
   return password;
 
-}
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -21,7 +52,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
